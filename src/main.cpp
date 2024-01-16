6d430e383054fae9e142f49b61ad73d4d9a2e6ef
MasonI2006 <masonistre25@students.casciahall.com>
2024-01-16 17:49:09 -0600

    added flex sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
+#include <Adafruit_ADS7830.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_ADS7830 ad7830;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+  ad7830.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+ /*uint8_t readFlex(int pin) {
+uint8_t analogvalue = analogRead(pin);
+  Serial.print(analogvalue);
+  return analogvalue;
+}*/ //DO NOT CALL FUNCTION IT WILL BREAK CODE, JUST FOLLOW THIS EXAMPLE WHEN IMPLEMENTING FLEX SENSOR
